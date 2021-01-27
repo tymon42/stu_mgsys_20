b39cd2e630466dc2e79fbf835c9e3381e18952c5
tymon42 <tymon42@outlook.com>
2021-01-27 13:44:06 +0800

    优化注释，新增数据结构长度检测。

--- index.c
+++ index.c
@@ -3,4 +3,5 @@
 #include<math.h>
 #include <stdlib.h>
 
-#define   COURSE_NUM 4                     /* 最多的考试科目数 */
+#define   COURSE_NUM 4  // 最多的考试科目数 
+#define   LEN sizeof(struct Student) // 数据结构的长度
