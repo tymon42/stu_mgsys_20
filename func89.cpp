6b34faebfc587ae1974a009119f0824e146c2101
tymon42 <tymon42@outlook.com>
2021-01-29 22:45:36 +0800

    修改密码函数

---
+++ func89.cpp
@@ -0,0 +1,12 @@
+#include "func89.h"
+
+// ÐÞ¸ÄÃÜÂë
+void UpdataPW()
+{
+    char a[20];
+    scanf("%s",a);
+    FILE *fp = fopen("ps.txt", "w");
+    fprintf(fp,"%s\n",a);	
+    fclose(fp);
+    return;
+}
---
+++ func89.h
@@ -0,0 +1,8 @@
+#ifndef __FUNC89_H__
+#define __FUNC89_H__
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+void UpdataPW();
+
+#endif
