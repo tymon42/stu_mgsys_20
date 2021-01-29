c8d5fbe16f9672d6712d9ab219c4dbb8829e0005
tymon42 <tymon42@outlook.com>
2021-01-29 16:27:58 +0800

    添加测试文件

---
+++ test_file.cpp
@@ -0,0 +1,11 @@
+#include "stumgsys20.h"
+
+int main()
+{
+	// Node *list;
+	list = CreateInfoList();
+	printf("\t\t\t******************************************************************************\n");
+	// 读取学生信息文件
+	// readStuInfoFromFile(list)
+}
+	
