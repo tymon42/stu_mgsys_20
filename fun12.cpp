4c25e74cbd2fb804dd3afe93401364d87745f7bb
xiesnowt  <1172539309@qq.com>
2021-01-30 21:04:55 +0800

    fun12的修改

--- fun12.cpp
+++ fun12.cpp
@@ -2,51 +2,75 @@
 /*
 按姓名排序，输出
 按平均成绩排序，输出
-输出给定学院学生*/ 
-void sortOnName1(Node *p)//从小到大 按姓名 排序 
+*/ 
+//sum为学生总数 
+void sortOnName1(Node *p,int sum)//从小到大 按姓名 排序 
 {
 	Data *mid;
+	Node *px=p->next;
 	
-	while(p->next)
+	int i,j;
+	for(i=0;i<num;i++)
 	{
-		if(strcmp(p->data.name,p->next.data.name)>0)
+		for(j=0;j<num-i-1;j++)
 		{
-			*mid=p->data;
-			p->data=p->next.data;
-			p->next.data=*mid;
+				if(strcmp(p->data.name,p->next.data.name)>0)
+			{
+				*mid=p->data;
+				p->data=p->next.data;
+				p->next.data=*mid;
+			}
+			px=px->next;	
 		}
 		p=p->next;
+		px=p->next;
 	}
-} 
-void sortOnName2(Node *p)//从大到小 按姓名 排序 
-{
-	Data *mid;
-	
-	while(p->next)
-	{
-		if(strcmp(p->data->name,p->next->data->name)<0)
-		{
-			*mid=p->data;
-			p->data=p->next->data;
-			p->next->data=*mid;
-		}
-		p=p->next;
-	}
-} 
 
-void sortaverage(Node *p)  //按平均成绩从大到小排序 
+} 
+void sortOnName2(Node *p,int num)//从大到小 按姓名 排序 
 {
 	Data *mid;
+	Node *px=p->next;
 	
-	while(p->next)
+	int i,j;
+	for(i=0;i<num;i++)
 	{
-		if(p->data.average<p->next.data.average)
+		for(j=0;j<num-i-1;j++)
 		{
-			*mid=p->data;
-			p->data=p->next->data;
-			p->next.data=*mid;
+				if(strcmp(p->data.name,p->next.data.name)<0)
+			{
+				*mid=p->data;
+				p->data=p->next.data;
+				p->next.data=*mid;
+			}
+			px=px->next;	
 		}
 		p=p->next;
+		px=p->next;
+	}
+
+}
+
+void sortaverage(Node *p,int num)  //按平均成绩从大到小排序 
+{
+	Data *mid;
+	Node *px=p->next;
+	
+	int i,j;
+	for(i=0;i<num;i++)
+	{
+		for(j=0;j<num-i-1;j++)
+		{
+				if(p->data.average<p->next.data.average)
+			{
+				*mid=p->data;
+				p->data=p->next.data;
+				p->next.data=*mid;
+			}
+			px=px->next;	
+		}
+		p=p->next;
+		px=p->next;
 	}
 }
 
