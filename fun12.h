8659829d88dc427a65092a0ff9eac195066f36f3
tymon42 <tymon42@outlook.com>
2021-01-31 14:14:16 +0800

    完善形参

--- fun12.h
+++ fun12.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include "list.h"
 using namespace std;
-void sortOnName1(Node*);
-void sortOnName2(Node*);
-void sortaverage(Node*);
+void sortOnName1(Node*, int);
+void sortOnName2(Node*, int);
+void sortaverage(Node*, int);
 #endif
